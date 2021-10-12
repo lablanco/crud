34481658249daa6cd97954a26fe94b937feb2709
Soles Magicos <lablanco@yahoo.com>
2021-10-12 18:02:43 -0300

    12oct

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -3,3 +3,17 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fastifycrud')
     .then(() => console.log("MongoDB Connected...."))
     .catch((err) => console.log(err));
+
+// const username = "<username>";
+// const password = "<dbpassword>";
+// const cluster = "<cluster0>";
+// const dbname = "<dbname>";
+
+// mongoose.connect(
+//     `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`);
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//     console.log("Connected successfully");
+// });
